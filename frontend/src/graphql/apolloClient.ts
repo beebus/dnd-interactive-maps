32b01c2e40646f9f6698fa1574064641851a1dd7
Michael Beebower <3438663+beebus@users.noreply.github.com>
2026-05-16 23:44:45 -0500

    Removed react-scripts. Added Vite.

--- frontend/src/graphql/apolloClient.ts
+++ frontend/src/graphql/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:8000/graphql/',
+    uri: import.meta.env.VITE_GRAPHQL_URI || 'http://localhost:8000/graphql/',
   }),
   cache: new InMemoryCache(),
 });
